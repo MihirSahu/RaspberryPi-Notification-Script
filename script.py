98551695d84195eb551ef59b9730fb13804fbddd
Mihir Sahu <2002mihir@gmail.com>
2021-11-07 22:21:03 -0600

    Sleep reminder

--- script.py
+++ script.py
@@ -6,7 +6,10 @@ conn = http.client.HTTPSConnection("api.pushover.net:443")
 lastCheck = datetime.datetime.now().minute
 
 while True:
+	# Testing
 	#print(str(lastCheck) + " " + str((datetime.datetime.now().minute + 1) % 60))
+
+	"""
 	if datetime.datetime.now().minute == (lastCheck + 5) % 60:
 		conn.request("POST", "/1/messages.json",
 		
@@ -20,3 +23,17 @@ while True:
 		print(conn.getresponse().read())
 
 		lastCheck = (lastCheck + 1) % 60
+	"""
+
+	if (datetime.datetime.now().hour == 23) and (datetime.datetime.now().minute == 59):
+		conn.request("POST", "/1/messages.json",
+		
+		  urllib.parse.urlencode({
+		    "token": "",
+		    "user": "",
+		    "title": "Go to sleep!",
+		    "message": f"It's 12:00! Go to sleep or you'll regret it :p",
+		  }), { "Content-type": "application/x-www-form-urlencoded" })
+		
+		print(conn.getresponse().read())
+		time.sleep(60 - datetime.datetime.now().second)
